d4503ab36b64c8187611f8e956acdb01247b38eb
SalimFares4 <fares01@ads.uni-passau.de>
2023-11-18 11:08:50 +0100

    Threading problem is fixed

--- Code/PytQt5 GUI design.py
+++ Code/PytQt5 GUI design.py
@@ -1,7 +1,7 @@
-# Welcome to PyShine
-# This is part 16 of the PyQt5 learning series
-# Based on parameters, the GUI will plot Video using OpenCV and Audio using Matplotlib in PyQt5
-# We will use Qthreads to run the audio/Video streams
+# # Welcome to PyShine
+# # This is part 16 of the PyQt5 learning series
+# # Based on parameters, the GUI will plot Video using OpenCV and Audio using Matplotlib in PyQt5
+# # We will use Qthreads to run the audio/Video streams
 
 import sys
 import matplotlib
@@ -12,41 +12,47 @@ import matplotlib.ticker as ticker
 import queue
 import numpy as np
 import sounddevice as sd
-from PyQt5.QtGui import QImage
 from PyQt5 import QtCore, QtWidgets,QtGui
 from PyQt5 import uic
-from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtMultimedia import QAudioDeviceInfo,QAudio,QCameraInfo
+from PyQt5.QtCore import pyqtSlot, QFile, QTextStream
+from PyQt5.QtMultimedia import QAudioDeviceInfo,QAudio
 import time
-import queue
 import os
 import wave, pyaudio, pdb
-# import cv2,imutils
-from PyQt5.QtWidgets import QFileDialog
 import traceback
 import librosa
 import soundfile as sf
+import BreezeStyleSheets.breeze_resources
 from Models import *
+
+
+
 # For details visit pyshine.com
 
 
 
+params = {
+	"CHUNK": 1024,
+	"stt_model" : WhisperLargeV2(),
+	"ttt_model" : FastChatModel(),
+	"tts_model" : Bark(voice_preset = "v2/de_speaker_5"),
+	"welcome_message" : "Hallo, Ich heiße Vicuna, wie kann ich dir helfen?",
 
-input_audio_deviceInfos = QAudioDeviceInfo.availableDevices(QAudio.AudioInput)
+}
 
 class MplCanvas(FigureCanvas):
 	def __init__(self, parent=None, width=5, height=4, dpi=100):
-		fig = Figure(figsize=(width, height), dpi=dpi)
+		fig = Figure(figsize=(width, height), dpi=dpi, facecolor='black')
 		self.axes = fig.add_subplot(111)
 		self.axes.axis("off")
 		super(MplCanvas, self).__init__(fig)
 		fig.tight_layout()
 
 class PyShine_LIVE_PLOT_APP(QtWidgets.QMainWindow):
-	def __init__(self):
+	def __init__(self, params=params):
 		print("Initializing ... \n")
 		QtWidgets.QMainWindow.__init__(self)
-		self.ui = uic.loadUi('main.ui',self)
+		self.ui = uic.loadUi('new_main.ui',self)
 		self.resize(888, 600)
 		self.tmpfile = 'temp.wav'
 
@@ -54,69 +60,67 @@ class PyShine_LIVE_PLOT_APP(QtWidgets.QMainWindow):
 		icon.addPixmap(QtGui.QPixmap("PyShine.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
 		self.setWindowIcon(icon)
 		self.threadpool = QtCore.QThreadPool()	
-		self.threadpool.setMaxThreadCount(2)
 		self.stt_pool = QtCore.QThreadPool()
 		self.ttt_pool = QtCore.QThreadPool()
 		self.tts_pool = QtCore.QThreadPool()
-		self.CHUNK = 1024
+		self.params = params
+		self.CHUNK = self.params["CHUNK"]
 		self.q = queue.Queue(maxsize=self.CHUNK)
-		self.devices_list= []
-		for device in input_audio_deviceInfos:
-			self.devices_list.append(device.deviceName())
-		
-		self.comboBox.addItems(self.devices_list)
-		self.comboBox.currentIndexChanged['QString'].connect(self.update_now)
-		self.comboBox.setCurrentIndex(0)
 		
 		self.canvas = MplCanvas(self, width=5, height=4, dpi=100)
 		self.ui.gridLayout_4.addWidget(self.canvas, 2, 1, 1, 1)
 		self.reference_plot = None
 		
 
-		self.device = self.devices_list[0]
 		self.window_length = 1000
 		self.downsample = 1
 		self.channels = [1]
 		self.interval = 1
 		
+		self.samplerate = 16000
+		self.length  = int(self.window_length*self.samplerate/(1000*self.downsample))
 		
-		device_info =  sd.query_devices(self.device, 'input')
-		
-		self.samplerate = device_info['default_samplerate']
-		length  = int(self.window_length*self.samplerate/(1000*self.downsample))
-		sd.default.samplerate = self.samplerate
-		
-		self.plotdata =  np.zeros((length,len(self.channels)))
+		self.plotdata =  np.zeros((self.length,len(self.channels)))
 		self.timer = QtCore.QTimer()
-		self.timer.setInterval(self.interval) #msec
+		self.timer.setInterval(30) #msec
 		self.timer.timeout.connect(self.update_plot)
 		self.timer.start()
 		self.data=[0]
-		self.lineEdit.textChanged['QString'].connect(self.update_window_length)
-		self.lineEdit_2.textChanged['QString'].connect(self.update_sample_rate)
-		self.lineEdit_3.textChanged['QString'].connect(self.update_down_sample)
-		self.lineEdit_4.textChanged['QString'].connect(self.update_interval)
-		self.speaking_allowed = False
+
 		self.record_allowed = True
 		self.query_allowed = False
 		self.generating_allowed = False
+		self.plotting = True
 		self.stopped = False
-		# self.pushButton.clicked.connect(self.start_worker_1_mic)
-		self.pushButton.clicked.connect(self.start_app)
-		self.pushButton_2.clicked.connect(self.stop_worker)
-		self.stt_model = WhisperTiny()
-		self.ttt_model = FastChatModel()
-		self.tts_model = SpeechT5()
+		self.speaking_allowed =True
+
+		self.stt_model = self.params["stt_model"]
+		self.ttt_model = self.params["ttt_model"]
+		self.tts_model = self.params["tts_model"]
+
 		self.response=""
 		self.query=""
 		self.filepath=""
-		print("Initialiazing is finished.\n")
+
+
+		self.label_6.setText("BOT Status:   IDLE  ... ")		
+		self.welcome_message = self.params["welcome_message"]
+		self.ttt_model.conv.append_message(self.ttt_model.conv.roles[1], self.welcome_message)
+		self.filepath = self.generate_audio(self.welcome_message)
 		
-	
-	def start_app(self):
-		self.stopped = False
-		self.record_allowed = True
-		self.start_worker_1_mic()
+		print("Initialiazing is finished.\n")
+		self.pushButton_2.clicked.connect(self.stop_worker)
+		self.pushButton.clicked.connect(self.start_worker_4_speaker)
+		
+
+
+	def display_status(self, text):
+		self.label_6.setText(text)
+
+	def update_status(self, text):
+		status_worker = Worker(self.display_status, text)
+		self.threadpool.start(status_worker)
+		
 	
 	def record_speech(self):
 		curr_time = time.time()
@@ -132,21 +136,11 @@ class PyShine_LIVE_PLOT_APP(QtWidgets.QMainWindow):
 		print("start_worker_1 is reached\n")
 		if self.record_allowed is True and self.stopped is False:
 			print("Recording is allowed\n")
-			self.lineEdit.setEnabled(False)
-			self.lineEdit_2.setEnabled(False)
-			self.lineEdit_3.setEnabled(False)
-			self.lineEdit_4.setEnabled(False)
-			self.comboBox.setEnabled(False)
-			self.pushButton.setEnabled(False)
-			# self.canvas.axes.clear()
-			self.mic_worker = STTWorker(self.record_speech)
-			self.mic_worker.signals.finished.connect(self.start_worker_2_vicuna)
-			# self.mic_worker.signals.result.connect(self.start_worker_2_vicuna)
-			# self.threadpool.start(self.mic_worker)
-			self.stt_pool.start(self.mic_worker)	
+			mic_worker = STTWorker(self.record_speech)
+			mic_worker.signals.start.connect(self.update_status)
+			mic_worker.signals.finished.connect(self.start_worker_2_vicuna)
+			self.stt_pool.start(mic_worker)	
 
-			# self.reference_plot = None
-			self.timer.setInterval(self.interval) #msec
 		else:
 			print("Recording isn't allowed\n")
 		print("start_worker_1 is finished\n")
@@ -155,8 +149,7 @@ class PyShine_LIVE_PLOT_APP(QtWidgets.QMainWindow):
 	def query_vicuna(self, query:str):
 		curr_time = time.time()
 		print("Querying Vicuna ...\n")
-		# self.response = self.ttt_model.generate_start(query)
-		self.response = self.ttt_model.run(query)
+		self.response = self.ttt_model.generate_start(query)
 		print(self.response)
 		self.query_allowed = False
 		self.generating_allowed = True
@@ -167,13 +160,17 @@ class PyShine_LIVE_PLOT_APP(QtWidgets.QMainWindow):
 		
 		if emit_sig==1 and self.query_allowed and self.stopped is False:
 			vicuna_worker = VicunaWorker(self.query_vicuna, query=self.query)
+			vicuna_worker.signals.start.connect(self.update_status)
 			vicuna_worker.signals.finished.connect(self.start_worker_3_tts)
-			# self.threadpool.start(vicuna_worker)
 			self.ttt_pool.start(vicuna_worker)
+		elif emit_sig!=1:
+			print("The mic wasn't able to record!\n")
+		
+		elif not self.query_allowed:
+			print("Vicuna isn't available!\n")
 		else:
-			print("The mic wasn't able to record or Vicuna isn't available!\n")
-		
-		
+			print("Unknown problem while querying! \n")
+
 	def generate_audio(self, response):
 		curr_time=time.time()
 		self.filepath = self.tts_model.run(response)
@@ -185,24 +182,26 @@ class PyShine_LIVE_PLOT_APP(QtWidgets.QMainWindow):
 	def start_worker_3_tts(self, emit_sig):
 		if emit_sig ==1 and self.generating_allowed and self.stopped is False:
 			tts_worker = TTSWorker(self.generate_audio, response=self.response)
+			tts_worker.signals.start.connect(self.update_status)
 			tts_worker.signals.finished.connect(self.start_worker_4_speaker)
-
-			# self.threadpool.start(tts_worker)
 			self.tts_pool.start(tts_worker)
 		else:
 			print("Vicuna wasn't able to process your query!\n")
 	
+	def generate_plotdata(self):
+		if not self.q.empty():
+			data = self.q.get_nowait()
+			return data
+		else:
+			return np.zeros((self.CHUNK,len(self.channels)))
 		
-
 	def getAudio(self, filepath):
 		curr_time=time.time()
-		if self.speaking_allowed is True:
-			# QtWidgets.QApplication.processEvents()	
+		if self.speaking_allowed is True:	
 			CHUNK = self.CHUNK
 			x,_ = librosa.load(filepath, sr=16000)
 			sf.write('tmp.wav', x, 16000)
 			wf = wave.open('tmp.wav', 'rb')
-			# wf = wave.open(self.tmpfile, 'rb')
 			p = pyaudio.PyAudio()
 			stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
 							channels=wf.getnchannels(),
@@ -211,10 +210,9 @@ class PyShine_LIVE_PLOT_APP(QtWidgets.QMainWindow):
 							frames_per_buffer=CHUNK)
 			self.samplerate = wf.getframerate()
 			sd.default.samplerate = self.samplerate
-			while(self.speaking_allowed is True):
-				
-				# QtWidgets.QApplication.processEvents()    
-				data = wf.readframes(CHUNK)
+			data = wf.readframes(CHUNK)
+			self.label_6.setText("BOT Status:   SPEAKING  ... ")
+			while(self.speaking_allowed is True and data):
 				audio_as_np_int16 = np.frombuffer(data, dtype=np.int16)
 				audio_as_np_float32 = audio_as_np_int16.astype(np.float32)
 				# Normalise float32 array                                                   
@@ -223,27 +221,17 @@ class PyShine_LIVE_PLOT_APP(QtWidgets.QMainWindow):
 				try:
 					self.q.put_nowait(audio_normalised)
 					stream.write(data)
-				
-					# if self.speaking_allowed is False:
-					# 	break
+					data = wf.readframes(CHUNK)
 				except:
 					self.speaking_allowed = False
-					self.q.queue.clear()
-
 			
-		self.pushButton.setEnabled(True)
-		self.lineEdit.setEnabled(True)
-		self.lineEdit_2.setEnabled(True)
-		self.lineEdit_3.setEnabled(True)
-		self.lineEdit_4.setEnabled(True)
-		self.comboBox.setEnabled(True)	
 		self.speaking_allowed=False
 		self.record_allowed = True
 		print("Playing and Plotting took {:.2f}s to finish\n".format(time.time()-curr_time))
 
 
 	def start_worker_4_speaker(self, emit_sig):
-		if emit_sig==1 and self.speaking_allowed and self.stopped is False:
+		if self.speaking_allowed and self.stopped is False:
 			speaker_worker = Worker(self.getAudio, filepath=self.filepath)
 			speaker_worker.signals.finished.connect(self.start_worker_1_mic)
 			self.threadpool.start(speaker_worker)
@@ -251,64 +239,33 @@ class PyShine_LIVE_PLOT_APP(QtWidgets.QMainWindow):
 			print("The model couldn't generate a speech from the text response\n")
 
 	def stop_worker(self):
+		self.label_6.setText("BOT Status:   STOPPED  ... ")
 		self.stopped = True
 		self.record_allowed = False
 		self.query_allowed = False
 		self.generating_allowed = False
 		self.speaking_allowed = False
+		self.plotting = False
 		print("You have stopped all threads!\n")
-		self.pushButton.setEnabled(True)
-		self.lineEdit.setEnabled(True)
-		self.lineEdit_2.setEnabled(True)
-		self.lineEdit_3.setEnabled(True)
-		self.lineEdit_4.setEnabled(True)
-		self.comboBox.setEnabled(True)	
 		with self.q.mutex:
 			self.q.queue.clear()
-		# self.threadpool.close()
 		
-	def update_now(self,value):
-		self.device = self.devices_list.index(value)
-		
-
-	def update_window_length(self,value):
-		self.window_length = int(value)
-		length  = int(self.window_length*self.samplerate/(1000*self.downsample))
-		self.plotdata =  np.zeros((length,len(self.channels)))
-		
-
-	def update_sample_rate(self,value):
-		self.samplerate = int(value)
-		sd.default.samplerate = self.samplerate
-		length  = int(self.window_length*self.samplerate/(1000*self.downsample))
-		self.plotdata =  np.zeros((length,len(self.channels)))
-		
-	
-	def update_down_sample(self,value):
-		self.downsample = int(value)
-		length  = int(self.window_length*self.samplerate/(1000*self.downsample))
-		self.plotdata =  np.zeros((length,len(self.channels)))
-	
-
-	def update_interval(self,value):
-		self.interval = int(value)
-		
-		
+			
 
 	def update_plot(self):
 		try:
-			
-			threads_num = self.threadpool.activeThreadCount() + self.stt_pool.activeThreadCount() + self.ttt_pool.activeThreadCount() + self.tts_pool.activeThreadCount()
+			if self.timer.isActive():
+				threads_num = self.threadpool.activeThreadCount() + self.stt_pool.activeThreadCount() + self.ttt_pool.activeThreadCount() + self.tts_pool.activeThreadCount() + 1
+			else:
+				threads_num = self.threadpool.activeThreadCount() + self.stt_pool.activeThreadCount() + self.ttt_pool.activeThreadCount() + self.tts_pool.activeThreadCount()
 			print('ACTIVE THREADS:',threads_num,end=" \r")
-			while  self.speaking_allowed is True:
-				# QtWidgets.QApplication.processEvents()	
+			if  self.plotting is True:
 				try: 
-					self.data = self.q.get_nowait()
+					self.data = self.generate_plotdata()
 					
-					
-				except queue.Empty:
-					break
-				
+				except:
+					pass
+
 				shift = len(self.data)
 				self.plotdata = np.roll(self.plotdata, -shift,axis = 0)
 				self.plotdata = self.data
@@ -321,16 +278,12 @@ class PyShine_LIVE_PLOT_APP(QtWidgets.QMainWindow):
 					self.reference_plot = plot_refs[0]	
 				else:
 					self.reference_plot.set_ydata(self.ydata)
-					
-
 			
 			self.canvas.axes.yaxis.grid(True,linestyle='--')
 			start, end = self.canvas.axes.get_ylim()
 			self.canvas.axes.yaxis.set_ticks(np.arange(start, end, 0.1))
 			self.canvas.axes.yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.1f'))
 			self.canvas.axes.set_ylim( ymin=-1, ymax=1)
-
-
 
 			self.canvas.draw()
 		except Exception as e:
@@ -354,12 +307,12 @@ class Worker(QtCore.QRunnable):
 
 	@pyqtSlot()
 	def run(self):
-
 		self.function(*self.args, **self.kwargs)
 		self.signals.finished.emit()			
 	
 
 class STTSignals(QtCore.QObject):
+	start = QtCore.pyqtSignal(str)
 	finished = QtCore.pyqtSignal(int)
 	result = QtCore.pyqtSignal(str)
 
@@ -375,6 +328,7 @@ class STTWorker(QtCore.QRunnable):
 
 	@pyqtSlot()
 	def run(self):
+		self.signals.start.emit("BOT Status:   lISTENING  ... ")
 		try:
 			result = self.function(*self.args, **self.kwargs)
 		except:
@@ -389,7 +343,7 @@ class VicunaSignals(QtCore.QObject):
 	error = QtCore.pyqtSignal(tuple)
 	result = QtCore.pyqtSignal(str)
 	progress = QtCore.pyqtSignal(str)
-	start = QtCore.pyqtSignal()
+	start = QtCore.pyqtSignal(str)
 
 # www.pyshine.com
 class VicunaWorker(QtCore.QRunnable):
@@ -403,7 +357,7 @@ class VicunaWorker(QtCore.QRunnable):
 
 	@pyqtSlot()
 	def run(self):
-		self.signals.start.emit()
+		self.signals.start.emit("BOT Status:   PROCESSING  ... ")
 		# Retrieve args/kwargs here; and fire processing using them
 		try:
 			result = self.function(*self.args, **self.kwargs)
@@ -423,6 +377,7 @@ class VicunaWorker(QtCore.QRunnable):
 class TTSSignals(QtCore.QObject):
 	finished = QtCore.pyqtSignal(int)
 	# result = QtCore.pyqtSignal(str)
+	start = QtCore.pyqtSignal(str)
 
 # www.pyshine.com
 class TTSWorker(QtCore.QRunnable):
@@ -435,6 +390,7 @@ class TTSWorker(QtCore.QRunnable):
 		self.signals = TTSSignals()
 	@pyqtSlot()
 	def run(self):
+		self.signals.start.emit("BOT Status:   GENERATING  ... ")
 		try:
 			result = self.function(*self.args, **self.kwargs)
 			# self.signals.result.emit(result)
@@ -444,8 +400,20 @@ class TTSWorker(QtCore.QRunnable):
 			self.signals.finished.emit(1)
 		
 
-app = QtWidgets.QApplication(sys.argv)
-mainWindow = PyShine_LIVE_PLOT_APP()
-mainWindow.show()
-sys.exit(app.exec_())
+def main():
+	app = QtWidgets.QApplication(sys.argv)
 
+	# set stylesheet
+	file = QFile(":/dark/stylesheet.qss")
+	file.open(QFile.ReadOnly | QFile.Text)
+	stream = QTextStream(file)
+	app.setStyleSheet(stream.readAll())
+	# code goes here
+	mainWindow = PyShine_LIVE_PLOT_APP()
+	mainWindow.showMaximized() 
+
+	app.exec_()
+
+
+
+main()
